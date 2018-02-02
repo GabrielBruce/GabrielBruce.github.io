9cd1b1d810356f5abdd34e3098520217e45ef43f
Gabriel Bruce <johngabrielbruce@gmail.com>
2018-02-02 11:43:42 -0600

    moved some responsive menu changes from js to css

--- main.js
+++ main.js
@@ -3,14 +3,10 @@ function navMenu(){
     var menu = document.getElementById("dropdown-menu");
     if (menu.className == ("")){
         menu.className += " responsive";
-        icon.style.setProperty("bottom", "2px");
-        icon.style.setProperty("top", "auto");
         icon.innerHTML = "&#9650";
     }
     else {
         menu.className = "";
-        icon.style.setProperty("top", "2px");
-        icon.style.setProperty("bottom", "auto");
         icon.innerHTML = "&#9660";
     }
 }
